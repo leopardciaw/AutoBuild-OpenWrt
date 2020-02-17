71913e9a0c9c491d54eff6c24936aa40b92f8c2f
leopardciaw <47556424+leopardciaw@users.noreply.github.com>
2020-02-17 08:56:35 +0800

    Update customizek2p.sh

--- customizek2p.sh
+++ customizek2p.sh
@@ -7,8 +7,8 @@
 #=================================================
 # Modify default IP@sed '1,3s/my/your/g
 sed -i 's/192.168.1.1/192.168.6.1/g' openwrt/package/base-files/files/bin/config_generate
-sed -i '611s/15744/32448/' target/linux/ramips/image/mt7621
-#rm -rf openwrt/target/linux/ramips/image/mt7621.mk
+#sed -i '611s/15744/32448/' target/linux/ramips/image/mt7621
+rm -rf openwrt/target/linux/ramips/image/mt7621.mk
 rm -rf openwrt/target/linux/ramips/dts/mt7元一个月621_phicomm_k2p.dts
-#cp -rf ./backk2p.d/mt7621.mk openwrt/target/linux/ramips/image/
+cp -rf ./backk2p.d/mt7621.mk openwrt/target/linux/ramips/image/
 cp -rf ./backk2p.d/mt7621_phicomm_k2p.dts openwrt/target/linux/ramips/dts/
